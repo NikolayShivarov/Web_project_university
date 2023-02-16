00e7cc969c1a0d02834d1dba36fc459eacfa038c
todorakisa <todor.domitrov2000@gmail.com>
2023-02-16 19:12:48 +0200

    removed redundant comments

--- src/get_categories.php
+++ src/get_categories.php
@@ -1,13 +1,5 @@
 <?php 
 require_once "db.php";
-// $conn = mysqli_connect("localhost", "root", "", "webproject") ;
-// $result = mysqli_query($conn, "SELECT distinct category FROM questions");
-// $data = array();
-// while ($row = mysqli_fetch_object($result))
-// {
-//     array_push($data, $row);
-// }
-
 $db = new Database();
 $query = $db->selectCategoriesQuery();
 $data = $query['data']->fetchAll(PDO::FETCH_ASSOC);
